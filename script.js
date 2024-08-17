8329beadc85a9c35f40380544ea16d352cfc3ca0
Nemesis502 <klawes-bastian@t-online.de>
2024-08-17 22:00:19 +0200

    clean up code

--- script.js
+++ script.js
@@ -19,45 +19,28 @@ function showQuestion() {
     document.getElementById("endScreen").style = "";
     document.getElementById("questionBody").style = "display: none";
   } else {
-    let percent = (currentQuestion + 1) / questions.length;
-    percent = Math.round(percent * 100);
-    document.getElementById("progress-bar").innerHTML = `${percent} %`;
-    document.getElementById("progress-bar").style.width = `${percent}%`;
-    console.log("Fortschritt:", percent);
-
-    let question = questions[currentQuestion];
-    document.getElementById("question-number").innerHTML = currentQuestion + 1;
-    document.getElementById("questionText").innerHTML = question["question"];
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];
+    updateProgressBar();
+    showNextQuestion();
   }
 }
 
 function answer(selcction) {
   let question = questions[currentQuestion]; // weiß damit welche Frage ansteht
-  // console.log(selcction);
   let selectedQuestionNumber = selcction.slice(-1); // entfernt den letzten Buschstaben aus z.b. "answer_2, in dem Fall die 2". Je nachdem welche Antwort angeklickt wird.
-  // console.log(selectedQuestionNumber);
-  // console.log(question["right_answer"]);
-
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
   document.getElementById("next-button").disabled = false;
 
   if (selectedQuestionNumber == question["right_answer"]) {
     //prüft ob der entfernte Buchstabe mit der richten Antwort übereinstimmt
-    // console.log("Richtige Antwort!");
     document.getElementById(selcction).parentNode.classList.add("bg-success");
-    audio_Success.play()
+    audio_Success.play();
     rightQuestions++;
   } else {
-    // console.log("Falsche Antwort");
     document.getElementById(selcction).parentNode.classList.add("bg-danger");
     document
       .getElementById(idOfRightAnswer)
       .parentNode.classList.add("bg-success");
-      audio_Fail.play()
+    audio_Fail.play();
   }
 }
 
@@ -91,5 +74,23 @@ function restartGame() {
   rightQuestions = 0;
   document.getElementById("endScreen").style = "display: none"; //Endscreen ausblenden
   document.getElementById("questionBody").style = ""; // Fragen wieder anzeigen
-  render()
+  render();
+}
+
+function showNextQuestion() {
+  let question = questions[currentQuestion];
+  document.getElementById("question-number").innerHTML = currentQuestion + 1;
+  document.getElementById("questionText").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function updateProgressBar() {
+  let percent = (currentQuestion + 1) / questions.length;
+  percent = Math.round(percent * 100);
+  document.getElementById("progress-bar").innerHTML = `${percent} %`;
+  document.getElementById("progress-bar").style.width = `${percent}%`;
+  console.log("Fortschritt:", percent);
 }
